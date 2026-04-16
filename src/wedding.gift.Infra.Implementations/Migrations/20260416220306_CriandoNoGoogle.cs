85dca6a7a9bf716226602fb7a9c90a14bc23a8b4
Nathan Galvão <n.galvao@influecy.me>
2026-04-16 19:05:44 -0300

    Atualiza conexões e adiciona migração inicial Google
    Atualizadas as strings de conexão para novo servidor e usuário em todos os ambientes. Adicionado serviceDependencies.json com dependência "apis1". Incluída migração 20260416220306_CriandoNoGoogle com estrutura das entidades Contribution e Gift e seed de dados para Gifts.

---
+++ src/wedding.gift.Infra.Implementations/Migrations/20260416220306_CriandoNoGoogle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wedding.gift.Infra.Implementations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoNoGoogle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
