d96911df65cb814447ed9f8d09df004abc8c64af
Nathan Galvão <n.influency.me>
2026-04-16 22:47:24 -0300

    Atualiza conexão e adiciona migração RecriandoBanco
    Atualiza a string de conexão DefaultConnection nos arquivos de configuração para usar o IP 35.193.28.139. Adiciona a migração "RecriandoBanco" do EF Core, incluindo o modelo das entidades Gift e Contribution, seus relacionamentos e dados iniciais para a tabela Gifts.

---
+++ src/wedding.gift.Infra.Implementations/Migrations/20260417014604_RecriandoBanco.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wedding.gift.Infra.Implementations.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecriandoBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
